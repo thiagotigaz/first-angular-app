ac9a10f639247792362ebd5818fb9ac1bac2644e
Thiago Lima <thiagotigaz@gmail.com>
2015-11-26 02:09:25 +0000

    factored service, added remove, added add

--- js/controllers/product.js
+++ js/controllers/product.js
@@ -1,4 +1,18 @@
 scApp.controller('ProductCtrl', ['$scope', 'ProductService', function($scope, ProductService){
-	$scope.product;
+
 	$scope.products = ProductService.getProducts();
+	$scope.p = [];
+
+	$scope.addProduct = function(){
+		ProductService.addProduct($scope.p);
+		$scope.p = [];
+	}
+
+	$scope.removeProduct = function(product){
+		ProductService.removeProduct(product);
+	}
+
+	$scope.clearForm = function(){
+		$scope.p = [];
+	};
 }]);
--- js/services/product.service.js
+++ js/services/product.service.js
@@ -2,54 +2,53 @@ scApp.service('ProductService', ['$http', function($http){
 	
 	var products = [
 	{
+		id: 1,
 		name: 'Produto 1',
 		description: 'Descricao 1',
 		cost:10,
 		price:20
 	},
 	{
+		id: 2,
 		name: 'Produto 2',
 		description: 'Descricao 2',
 		cost:10,
 		price:20
 	},
 	{
+		id: 3,
 		name: 'Produto 2',
 		description: 'Descricao 2',
 		cost:10,
 		price:20
 	},
 	{
+		id: 4,
 		name: 'Produto 3',
 		description: 'Descricao 3',
 		cost:10,
 		price:20
 	},
 	{
+		id: 5,
 		name: 'Produto 4',
 		description: 'Descricao 4',
 		cost:10,
 		price:20
 	}
-	]
+	];
 
-	function addProduct(product){
+	this.addProduct = function addProduct(product){
 		products.push(product);
-	}
+	};
 
-	function removeProduct(product){
+	this.removeProduct = function removeProduct(product){
 		var index = products.indexOf(product);
-		products.splice(index, 1);
+		;products.splice(index, 1);
 	}
 
-	function getProducts(){ 
+	this.getProducts = function getProducts(){ 
 		return products;
-	}
-
-	return { 
-		addProduct: addProduct,
-		removeProduct : removeProduct,
-		getProducts : getProducts
-	}
+	};
 
 }]);
